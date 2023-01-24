d1a6be81acdbaf223b986050c2440640a2d9fa78
obel <rdebebe@kifiya.et>
2023-01-24 19:47:33 +0300

    Updated

--- 0x00-Webpack/task_0/src/index.js
+++ 0x00-Webpack/task_0/src/index.js
@@ -1,5 +1,6 @@
-import $ from 'jquery';
+"use strict";
+const $ = require("jquery");
 
-$('body').append('<p>Holberton Dashboard</p>');
-$('body').append('<p>Dashboard data for the students</p>');
-$('body').append('<p>Copyright - Holberton School</p>');
+$("body").append("<p>Holberton Dashboard</p>");
+$("body").append("<p>Dashboard data for the students</p>");
+$("body").append("<p>Copyright - Holberton School</p>");
